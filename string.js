f03821c0a55ae36db4512b503668a068cd0a1aab
gidi2904 <akinolagoodness0@gmail.com>
2024-04-16 13:05:09 +0100

    add. to file

---
+++ string.js
@@ -0,0 +1,15 @@
+// string method 
+const sentence =`the quick brown fox jumps over the lazy dog`
+
+// length of a string 
+console.log(sentence.length)
+
+// convert to uppercase
+document.write(sentence.toUpperCase())
+
+// covert to lowercase
+document.write(sentence.toLowercase())
+
+// cut some part of a string 
+console.log(sentence.slice(0,10))
+
---
+++ two.js
@@ -0,0 +1,23 @@
+let num1
+let num2
+let num12
+const num3 =77
+
+let $name
+let _name
+let _
+let $
+
+num1=80
+num12=900
+let myName = 'Goodness'
+
+let fullName ='Goodness Akinola'
+
+let workPlace = 'univelcity'
+
+let isMale = true
+
+let statement =`my name is ${fullName} and i was born in ${new Date().getFullYear() -37} and i work at ${workPlace},
+
+console.log(statement)
